65cbe6e3b5520a6cf15aeb757e0880307aad33f4
Alexandre Manso <alexandre.manso47@gmail.com>
2024-06-16 18:51:50 +0200

    week7

---
+++ Week7/Day2/ExercicesXP/Exercice1.sql
@@ -0,0 +1,35 @@
+-- Database: public
+
+-- DROP DATABASE IF EXISTS public;
+
+--CREATE DATABASE public
+--    WITH
+--    OWNER = alexandremanso
+--    ENCODING = 'UTF8'
+--    LC_COLLATE = 'C'
+--    LC_CTYPE = 'C'
+--   TABLESPACE = pg_default
+--    CONNECTION LIMIT = -1
+--    IS_TEMPLATE = False;
+
+--SELECT * 
+--FROM customers;
+
+--SELECT * 
+--FROM items
+--ORDER BY price ASC;
+
+--SELECT * 
+--FROM items
+--WHERE price >= 80
+--ORDER BY price DESC;
+
+--SELECT firstname, lastname
+--FROM customers
+--ORDER BY firstname ASC
+--LIMIT 3;
+
+--SELECT lastname
+--FROM customers
+--ORDER BY lastname DESC;
+
